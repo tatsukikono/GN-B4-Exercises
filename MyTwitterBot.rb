ee804b24ae6340fc462919eb2541bd562c4b0111
kono <kono@kawasemi2.swlab.cs.okayama-u.ac.jp>
2013-04-10 16:34:59 +0900

    Add a function to reply

--- MyTwitterBot.rb
+++ MyTwitterBot.rb
@@ -1,7 +1,28 @@
 # -*- coding: utf-8 -*-
 require './TwitterBot.rb' # TwitterBot.rbの読み込み
-
+require "stringio"
 #---------- MyTwitterBot ----------                                                                         
 class MyTwitterBot < TwitterBot
   # 機能を追加
 end
+
+twitter = MyTwitterBot.new
+
+# twitter.tweet("message")
+
+tweets = twitter.get_tweet
+
+#---と言ってTweet---
+  tweets.each do |tweet|
+    if /(.*)と言って/ =~ tweet["message"]
+      twitter.tweet("#{$1}  tweet by bot")
+    end
+  end
+
+
+#---誕生日Tweet---
+#File.open("birthday.txt") do |io|
+  
+ # p io.read
+
+  #end
